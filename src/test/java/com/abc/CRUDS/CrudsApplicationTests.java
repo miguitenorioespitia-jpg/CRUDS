fa1264fb9b57556ebcdb5e1056b2f0b03866ba1b
Miguel Tenorio <migueltenorioespitia@gmail.com>
2025-10-29 22:13:33 -0500

    Commit inicial

---
+++ src/test/java/com/abc/CRUDS/CrudsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abc.CRUDS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
